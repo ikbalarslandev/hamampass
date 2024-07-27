229c14247d15262d09764ede512ef4f0573e7b25
ikbal arslan <ikbalarslandev@gmail.com>
2024-07-27 14:35:22 +0300

    i18n setup

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,8 +1,11 @@
 import HomePage from "@/components/home";
+import { useTranslations } from "next-intl";
 
 export default function Home() {
+  const t = useTranslations("Index");
   return (
     <main className="max-w-full">
+      <h1>{t("title")}</h1>
       <HomePage />
     </main>
   );
--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,7 @@
+import createNextIntlPlugin from "next-intl/plugin";
+
+const withNextIntl = createNextIntlPlugin();
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
@@ -9,4 +13,4 @@ const nextConfig = {
   },
 };
 
-export default nextConfig;
+export default withNextIntl(nextConfig);
