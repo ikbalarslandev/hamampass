09690e1a24efabb6cd5a2dd26a5f54b91ead893d
ikbal arslan <ikbalarslandev@gmail.com>
2024-07-30 00:49:40 +0300

    contact info

--- components/single-property/location.tsx
+++ components/single-property/location.tsx
@@ -9,6 +9,9 @@ const LocationComponent = ({ contact }: { contact: TContact }) => {
     <div className="flex flex-col gap-3 my-4">
       <hr />
       <h2 className="font-bold text-xl text-gray-600">{s("location")}</h2>
+      <p className="text-gray-700">
+        {s("tel")} {contact.phone}
+      </p>
       <div>
         <p className="text-gray-500">{contact.address}</p>
         <GoogleMapComponent contact={contact.location} />
