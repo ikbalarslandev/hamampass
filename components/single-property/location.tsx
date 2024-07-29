6a3d032bb590933276b925310f43e382f64b51de
ikbal arslan <ikbalarslandev@gmail.com>
2024-07-30 00:20:55 +0300

    map imporovement

--- components/single-property/location.tsx
+++ components/single-property/location.tsx
@@ -1,11 +1,18 @@
 import { TContact } from "@/types";
 import GoogleMapComponent from "./map";
+import { useTranslations } from "next-intl";
 
 const LocationComponent = ({ contact }: { contact: TContact }) => {
+  const s = useTranslations("single");
+
   return (
-    <div>
-      {contact.phone}
-      <GoogleMapComponent contact={contact.location} />
+    <div className="flex flex-col gap-3 my-4">
+      <hr />
+      <h2 className="font-bold text-xl text-gray-600">{s("location")}</h2>
+      <div>
+        <p className="text-gray-500">{contact.address}</p>
+        <GoogleMapComponent contact={contact.location} />
+      </div>
     </div>
   );
 };
