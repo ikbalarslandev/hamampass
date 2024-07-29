7d839d36c4a40f94a7140a1035164d8f819a9e7b
ikbal arslan <ikbalarslandev@gmail.com>
2024-07-29 23:10:12 +0300

    deployment bug fix

--- components/single-property/map.tsx
+++ components/single-property/map.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React from "react";
 import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
+import { useState, useEffect } from "react";
 
 const containerStyle = {
   width: "100%",
@@ -15,16 +16,24 @@ const center = {
 const GoogleMapComponent = () => {
   const googleMapsApiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY;
 
+  const [key, setKey] = useState(googleMapsApiKey);
+
+  useEffect(() => {
+    setKey(googleMapsApiKey);
+  }, [googleMapsApiKey]);
+
   if (!googleMapsApiKey) {
     return <div>Error: Google Maps API key is missing.</div>;
   }
 
   return (
-    <LoadScript googleMapsApiKey={googleMapsApiKey}>
-      <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={12}>
-        <Marker position={center} />
-      </GoogleMap>
-    </LoadScript>
+    key && (
+      <LoadScript googleMapsApiKey={key}>
+        <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={12}>
+          <Marker position={center} />
+        </GoogleMap>
+      </LoadScript>
+    )
   );
 };
 
