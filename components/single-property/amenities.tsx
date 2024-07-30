86a709d13ad810ff5511d8ee24efac2869e191aa
ikbal arslan <ikbalarslandev@gmail.com>
2024-07-30 13:48:40 +0300

    hydration bug fix

--- components/single-property/amenities.tsx
+++ components/single-property/amenities.tsx
@@ -10,12 +10,12 @@ const AmenityComponent = ({ data }: any) => {
     <div className="flex flex-col gap-2">
       <hr />
       <h2 className="font-bold text-xl text-gray-600">{s("amenity")}</h2>
-      <p className="flex flex-col gap-4 mt-2 ml-1">
+      <div className="flex flex-col gap-4 mt-2 ml-1">
         {data &&
           data.map((amenity: any, index: number) => (
             <HoverComponent key={index} amenity={amenity} />
           ))}
-      </p>
+      </div>
     </div>
   );
 };
