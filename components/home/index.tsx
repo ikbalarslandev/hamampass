32108b557c38d78dee8718960c39c0b641031d6c
ikbal arslan <ikbalarslandev@gmail.com>
2024-08-01 01:38:20 +0300

    sort params

--- components/home/index.tsx
+++ components/home/index.tsx
@@ -4,18 +4,25 @@ import Cards from "../cards";
 import { request } from "@/services/axios";
 import { TApiResponse } from "@/types";
 import SelectComponent from "./select";
+import { useSearchParams } from "next/navigation";
 
 const HomePage = () => {
   const [properties, setProperties] = useState<TApiResponse>(
     {} as TApiResponse
   );
+  const searchParams = useSearchParams();
 
   useEffect(() => {
+    const sortParam = searchParams.get("sort");
+
     const fetchProperties = async () => {
       try {
         const response = await request({
           type: "get",
           endpoint: "property",
+          params: {
+            sort: sortParam,
+          },
         });
 
         setProperties(Array.isArray(response.data.data) ? response.data : {});
@@ -25,7 +32,7 @@ const HomePage = () => {
     };
 
     fetchProperties();
-  }, []);
+  }, [searchParams]); // Dependency array includes searchParams
 
   return (
     <div>
