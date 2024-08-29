c10ff868743bd91d386cf016f0c52ba4676a407d
ikbal arslan <ikbalarslandev@gmail.com>
2024-08-29 03:50:36 +0300

    amenitites Drawer

--- components/single-property/details/amenities/index.tsx
+++ components/single-property/details/amenities/index.tsx
@@ -2,9 +2,12 @@
 
 import HoverComponent from "@/components/hover";
 import { useTranslations } from "next-intl";
+import AmenitiesDrawer from "./drawer";
+import { FaGreaterThan } from "react-icons/fa";
 
 const AmenityComponent = ({ data }: any) => {
   const title = useTranslations("home.filters.titles");
+  const firstThree = data.slice(0, 3);
 
   return (
     <section className="flex flex-col gap-2">
@@ -14,10 +17,19 @@ const AmenityComponent = ({ data }: any) => {
       </h2>
       <div className="flex flex-col gap-4 mt-2 ml-1">
         {data &&
-          data.map((id: any, index: number) => (
+          firstThree.map((id: any, index: number) => (
             <HoverComponent key={index} amenity={id} isDesc={true} />
           ))}
       </div>
+      <AmenitiesDrawer
+        trigger={
+          <button className="btn btn-primary w-full pr-4 text-xs font-semibold flex items-center gap-2 justify-end">
+            <span>View All Amenities</span>
+            <FaGreaterThan size={10} />
+          </button>
+        }
+        data={data}
+      />
     </section>
   );
 };
