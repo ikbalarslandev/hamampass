e003e892ad3e5adc436a75de35f2b454857991ef
ikbal arslan <ikbalarslandev@gmail.com>
2024-09-04 01:28:08 +0300

    home hero navigation bug

--- components/pages/home/index.tsx
+++ components/pages/home/index.tsx
@@ -2,7 +2,7 @@ import { DatePickerForm } from "@/components/pages/home/date-picker";
 
 const HomePage = () => {
   return (
-    <div className="flex-1 flex flex-col items-center justify-end pb-[calc(2rem + env(safe-area-inset-bottom)) ] gap-5">
+    <div className="flex-1 flex flex-col items-center justify-end pb-8 gap-5">
       <DatePickerForm />
     </div>
   );
