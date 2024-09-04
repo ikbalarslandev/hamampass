3e3e7983b4b2fceff64a1809b2b7a9794eb0e684
ikbal arslan <ikbalarslandev@gmail.com>
2024-09-04 18:45:02 +0300

    product drawer content improve

---
+++ components/pages/single-property/details/products/counter.tsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+const CounterComponent = ({ count, setCount }: any) => {
+  const increment = () => setCount(count + 1);
+  const decrement = () => setCount(count > 0 ? count - 1 : 0); // Prevents negative count
+
+  return (
+    <div className="flex items-center border  overflow-hidden  rounded-full shadow">
+      <button
+        className="py-1 px-3 ml-1 aspect-square border text-xl font-semibold border-r focus:outline-none rounded-full "
+        onClick={decrement}
+      >
+        -
+      </button>
+
+      <input
+        type="number"
+        className="w-12 text-center px-2 py-2 text-lg  focus:outline-none"
+        value={count}
+        readOnly
+      />
+      <button
+        className="py-1 px-3 mr-1 aspect-square bg-cyan-500 text-white text-lg font-semibold  focus:outline-none rounded-full"
+        onClick={increment}
+      >
+        +
+      </button>
+    </div>
+  );
+};
+
+export default CounterComponent;
--- components/pages/single-property/details/products/drawer.tsx
+++ components/pages/single-property/details/products/drawer.tsx
@@ -16,6 +16,14 @@ import { useTranslations } from "next-intl";
 import { useParams } from "next/navigation";
 import { GoDotFill } from "react-icons/go";
 import { productPrototypes } from "@/utils/prototypes";
+import moment from "moment";
+import { useEffect } from "react";
+import "moment/locale/tr";
+import "moment/locale/en-gb";
+import { Separator } from "@/components/ui/separator";
+import CounterComponent from "./counter";
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
 
 interface DrawerComponentProps {
   trigger: React.ReactNode;
@@ -26,62 +34,109 @@ const DrawerComponent = ({ trigger, data }: DrawerComponentProps) => {
   const t = useTranslations("home.product-type");
   const p = useTranslations("single.products");
   const { locale } = useParams();
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    if (locale === "tr") {
+      moment.locale("tr");
+    } else {
+      moment.locale("en-gb");
+    }
+  }, [locale]);
+
   return (
     <Drawer>
       <DrawerTrigger>{trigger}</DrawerTrigger>
-      <DrawerContent className="max-h-[90%] px-2">
+      <DrawerContent className="max-h-[90%] ">
         <DrawerHeader>
-          <DrawerTitle>{t(data.type.toString())}</DrawerTitle>
+          <DrawerTitle className="flex flex-col gap-2">
+            <span> {t(data.type.toString())}</span>
+            <span className="text-xs text-gray-400">
+              {moment(
+                JSON.parse(sessionStorage.getItem("selected-date") ?? "{}")
+              ).format("D MMMM YYYY")}
+            </span>
+          </DrawerTitle>
           <DrawerClose className="absolute top-2 right-5">X</DrawerClose>
+        </DrawerHeader>
 
-          <div className="text-left my-5">
-            <div className="flex items-center gap-2 justify-between ">
-              <p>
-                {p("adult")} :{" "}
-                <span className="text-xs">
-                  ({p("older")} {data.age ?? productPrototypes[data.type].age})
-                </span>
-              </p>
-              <p className="font-bold">
-                ₺{data.adult_price}
-                <span className="text-sm ml-1">TL</span>
-              </p>
-            </div>
-            {data.child_price ? (
-              <div className="flex items-center gap-2 justify-between ">
+        <div className="px-2">
+          <div className="text-left">
+            <Separator />
+            <div className="my-2 flex items-center justify-between">
+              <div className="flex flex-col  ">
                 <p>
-                  {p("child")} :{" "}
-                  <span className="text-xs">
-                    (0 - {data.age ?? productPrototypes[data.type].age} )
+                  <span className="font-medium mr-1">{p("adult")}</span>
+
+                  <span className="text-xs text-gray-400 font-light">
+                    ({p("older")} {data.age ?? productPrototypes[data.type].age}
+                    )
                   </span>
                 </p>
-                <p className="font-bold">
-                  ₺{data.child_price}
+                <p className="font-semibo">
+                  ₺{data.adult_price}
                   <span className="text-sm ml-1">TL</span>
                 </p>
               </div>
-            ) : null}
-          </div>
-        </DrawerHeader>
 
-        <div className="overflow-y-auto">
-          {(data[`detail_${locale}`].length > 0
-            ? data[`detail_${locale}`]
-            : productPrototypes[data.type][`detail_${locale}`]
-          ).map((item: string) => (
-            <DrawerDescription
-              key={item}
-              className="flex items-center gap-1 text-black text-md"
-            >
-              <GoDotFill size={10} />
-              <span>{item}</span>
-            </DrawerDescription>
-          ))}
+              <CounterComponent count={count} setCount={setCount} />
+            </div>
+
+            <Separator />
+
+            {/* {data.child_price ? (
+            <div className="flex items-center gap-2 justify-between ">
+              <p>
+                {p("child")} :{" "}
+                <span className="text-xs">
+                  (0 - {data.age ?? productPrototypes[data.type].age} )
+                </span>
+              </p>
+              <p className="font-bold">
+                ₺{data.child_price}
+                <span className="text-sm ml-1">TL</span>
+              </p>
+            </div>
+          ) : null} */}
+          </div>
+
+          <div className="overflow-y-auto mt-5">
+            {(data[`detail_${locale}`].length > 0
+              ? data[`detail_${locale}`]
+              : productPrototypes[data.type][`detail_${locale}`]
+            ).map((item: string) => (
+              <DrawerDescription
+                key={item}
+                className="flex items-center gap-1 text-black text-md"
+              >
+                <GoDotFill size={10} />
+                <span>{item}</span>
+              </DrawerDescription>
+            ))}
+          </div>
+          <div>
+            {data[`desc_${locale}`] ??
+              productPrototypes[data.type][`desc_${locale}`]}
+          </div>
         </div>
 
-        <DrawerFooter className=" px-0  ">
-          {data[`desc_${locale}`] ??
-            productPrototypes[data.type][`desc_${locale}`]}
+        <DrawerFooter className=" px-3 py-2 border-t mt-4 shadow-xl flex-row items-center  ">
+          <div className="flex flex-1 flex-col">
+            <p className="text-xs text-gray-600">Total</p>
+            <p className="font-bold text-xl">
+              ₺{data.adult_price * count}
+              <span className="text-sm ml-1">TL</span>
+            </p>
+            <p className="text-xs">no credit card required</p>
+          </div>
+
+          <Button
+            className={`rounded-xl px-8 bg-cyan-500 ${
+              count === 0 ? "opacity-50 cursor-not-allowed" : ""
+            }`}
+          >
+            Add to Card
+          </Button>
         </DrawerFooter>
       </DrawerContent>
     </Drawer>
