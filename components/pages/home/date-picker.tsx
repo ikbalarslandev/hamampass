1999f678fee96b09d488dc560408972ae04352c1
ikbal arslan <ikbalarslandev@gmail.com>
2024-09-04 03:25:26 +0300

    home fix

--- components/pages/home/date-picker.tsx
+++ components/pages/home/date-picker.tsx
@@ -5,6 +5,7 @@ import { format } from "date-fns";
 import { CalendarIcon } from "lucide-react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
+import { useState } from "react";
 
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
@@ -29,7 +30,7 @@ import { useParams } from "next/navigation";
 import moment from "moment";
 
 const FormSchema = z.object({
-  dob: z.date({
+  date: z.date({
     required_error: "A date of birth is required.",
   }),
 });
@@ -37,45 +38,46 @@ const FormSchema = z.object({
 export function DatePickerForm() {
   const router = useRouter();
   const { locale } = useParams();
+  const [open, setOpen] = useState(false);
 
   const form = useForm<z.infer<typeof FormSchema>>({
     resolver: zodResolver(FormSchema),
+    defaultValues: {
+      date: new Date(),
+    },
   });
 
   function onSubmit(data: z.infer<typeof FormSchema>) {
     router.push(`/${locale}/properties`);
-    sessionStorage.setItem("selected-date", JSON.stringify(data.dob));
+    sessionStorage.setItem("selected-date", JSON.stringify(data.date));
   }
-
-  const savedDate = JSON.parse(
-    sessionStorage.getItem("selected-date") || "null"
-  );
 
   return (
     <Form {...form}>
       <form
         onSubmit={form.handleSubmit(onSubmit)}
-        className="flex p-1 bg-white rounded-lg"
+        className="flex p-1 bg-white rounded-lg w-full"
       >
         <FormField
           control={form.control}
-          name="dob"
+          name="date"
           render={({ field }) => (
-            <FormItem className="flex flex-col">
-              <Popover>
+            <FormItem className="flex flex-col flex-1">
+              <Popover open={open} onOpenChange={setOpen}>
                 <PopoverTrigger asChild>
                   <FormControl>
                     <Button
                       variant="none"
                       className={cn(
-                        "w-[240px] pl-3 text-left font-normal flex items-center justify-start gap-2",
+                        "pl-3 text-left font-normal flex items-center justify-start gap-2",
                         !field.value && "text-muted-foreground"
                       )}
+                      onClick={() => setOpen(!open)}
                     >
                       <CalendarIcon className="h-4 w-4 opacity-50" />
                       {field.value
-                        ? moment(field.value).format("DD MMMM")
-                        : "Pick a date"}
+                        ? moment(field.value).format("D MMMM")
+                        : moment().format("D MMMM")}
                     </Button>
                   </FormControl>
                 </PopoverTrigger>
@@ -83,7 +85,10 @@ export function DatePickerForm() {
                   <Calendar
                     mode="single"
                     selected={field.value}
-                    onSelect={field.onChange}
+                    onSelect={(date) => {
+                      field.onChange(date);
+                      setOpen(false);
+                    }}
                     disabled={(date) => date < startOfDay(new Date())}
                     initialFocus
                   />
--- components/pages/home/index.tsx
+++ components/pages/home/index.tsx
@@ -2,7 +2,20 @@ import { DatePickerForm } from "@/components/pages/home/date-picker";
 
 const HomePage = () => {
   return (
-    <div className="flex-1 flex flex-col items-center justify-end mb-8 gap-5">
+    <div className="flex-1 flex flex-col items-center justify-end mb-8 gap-8 mx-5">
+      <div className=" w-full  ">
+        <div className="  text-white flex flex-col gap-4">
+          {" "}
+          <div className="flex flex-col text-5xl font-semibold gap-2 ">
+            <h1>Take a</h1>
+            <h1>hamampass</h1>
+          </div>
+          <p>
+            Experience İstanbul’s best hamams
+            <br /> no credit card required.
+          </p>
+        </div>
+      </div>
       <DatePickerForm />
     </div>
   );
