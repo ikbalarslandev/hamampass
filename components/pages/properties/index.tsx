e947b82580810b4077b0c30911d9050dd653d512
ikbal arslan <ikbalarslandev@gmail.com>
2024-09-08 19:52:33 +0300

    sort review frontend done bug fix

--- components/pages/properties/index.tsx
+++ components/pages/properties/index.tsx
@@ -3,21 +3,27 @@ import FilterComponent from "@/components/pages/properties/filters";
 import HomeTitle from "./title";
 import { getAllProperties } from "@/actions/property";
 import { NextRequest } from "next/server";
+import { TApiResponse, TProperty } from "@/types";
 
 const PropertiesPage = async () => {
   const req = new NextRequest(process.env.BASE_URL!!);
   const res = await getAllProperties(req);
 
-  if ("data" in res) {
-    var data = res.data;
-  }
+  // Type guard to check if the response has the expected shape
+  const isApiResponse = (response: any): response is TApiResponse => {
+    return response && "data" in response;
+  };
 
   return (
     <div>
       <FilterComponent />
       <hr />
       <HomeTitle />
-      <Cards serverProperties={data} />
+      {isApiResponse(res) ? (
+        <Cards serverProperties={res.data} />
+      ) : (
+        <p>Error loading properties</p>
+      )}
     </div>
   );
 };
