d0a0d2bb726b5b445f2b92edbdfc8d67a0315978
ikbal arslan <ikbalarslandev@gmail.com>
2024-09-20 19:07:06 +0300

    create seperade admin app bug

--- apps/admin/app/page.tsx
+++ apps/admin/app/page.tsx
@@ -3,8 +3,6 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <p className="text-4xl">Admin Page</p>
-
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
