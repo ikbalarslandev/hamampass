e90d44f4e9d2c97cf664da90a763de46a9bd48fa
ikbal arslan <ikbalarslandev@gmail.com>
2024-09-27 19:23:15 +0300

    bug fix

--- apps/admin/app/(auth)/login/page.tsx
+++ apps/admin/app/(auth)/login/page.tsx
@@ -1,9 +1,11 @@
+import { SignOut } from "@/components/auth/logout";
 import { SignIn } from "@/components/auth/sign-in";
 
 const Login = () => {
   return (
     <div>
       <SignIn />
+      <SignOut />
     </div>
   );
 };
