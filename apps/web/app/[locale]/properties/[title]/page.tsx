e2ce106ee237ec293618bfaaa06f0e662adeff50
ikbal arslan <ikbalarslandev@gmail.com>
2024-10-02 14:23:39 +0300

    metadata improve

--- apps/web/app/[locale]/properties/[title]/page.tsx
+++ apps/web/app/[locale]/properties/[title]/page.tsx
@@ -12,15 +12,20 @@ export async function generateMetadata({
 }): Promise<Metadata> {
   const { title } = params;
   const decodedTitle = decodeURI(title.replace(/-/g, " "));
+
+  const req = new NextRequest(`${process.env.BASE_URL}/api/property/${title}`);
+  const res = (await getPropertyByTitle(req)) as unknown as TProperty;
+  const image = res.photos[0];
+
   return {
     title: decodedTitle,
     openGraph: {
       title: decodedTitle,
       description: `Read more about ${decodedTitle}`,
-      url: `${process.env.BASE_URL}/en/properties/${title}`, // Replace with your page URL
+      url: `${process.env.BASE_URL}/en/properties/${title}`,
       images: [
         {
-          url: "https://duckduckgo.com/?q=car&t=brave&iax=images&ia=images&iai=https%3A%2F%2Fstatic1.hotcarsimages.com%2Fwordpress%2Fwp-content%2Fuploads%2F2019%2F07%2Fhttps-api.thedrive.com-wp-content-uploads-2018-09-jaguaripacefirstedition0131.jpgquality85-1.jpeg", // Update with dynamic or static image path
+          url: image,
           width: 1200,
           height: 630,
           alt: `${decodedTitle} image`,
@@ -32,9 +37,7 @@ export async function generateMetadata({
       card: "summary_large_image",
       title: decodedTitle,
       description: `Read more about ${decodedTitle}`,
-      images: [
-        "https://duckduckgo.com/?q=car&t=brave&iax=images&ia=images&iai=https%3A%2F%2Fstatic1.hotcarsimages.com%2Fwordpress%2Fwp-content%2Fuploads%2F2019%2F07%2Fhttps-api.thedrive.com-wp-content-uploads-2018-09-jaguaripacefirstedition0131.jpgquality85-1.jpeg",
-      ], // Update with the correct image URL
+      images: [image],
     },
   };
 }
