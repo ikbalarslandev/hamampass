af6d3a78cf0b8a4428387b443faaec102cff1929
ikbal arslan <ikbalarslandev@gmail.com>
2024-10-02 20:27:15 +0300

    Structured data test

--- apps/web/app/[locale]/page.tsx
+++ apps/web/app/[locale]/page.tsx
@@ -1,12 +1,28 @@
 import HeaderGeneral from "@/components/commons/header";
 import HomePage from "@/components/pages/home";
-import { StructuredData } from "@/utils/structuredData/home";
+import { jsonLd } from "@/utils/structuredData/home";
+
+import type { Metadata } from "next";
+
+export async function generateMetadata({
+  params,
+}: {
+  params: { locale: string };
+}): Promise<Metadata> {
+  console.log("params", params);
+
+  return {
+    other: {
+      "application/ld+json": JSON.stringify(jsonLd),
+    },
+  };
+}
 
 export default function Home({ params }: any) {
   const { locale } = params;
+
   return (
-    <main className="flex flex-col  h-svh ">
-      <StructuredData />
+    <main className="flex flex-col h-svh">
       <HeaderGeneral isBgNone={true} />
       <video
         autoPlay
