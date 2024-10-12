085c852da97b00391c0d1d71be6067558c79c39c
ikbal arslan <ikbalarslandev@gmail.com>
2024-10-12 15:49:18 +0300

    first setup for ui package

--- apps/web/app/[locale]/page.tsx
+++ apps/web/app/[locale]/page.tsx
@@ -1,12 +1,13 @@
 import HeaderGeneral from "@/components/commons/header";
 import HomePage from "@/components/pages/home";
+import { Button } from "@hamampass/ui/primitives/button";
 
 export default function Home({ params }: any) {
   const { locale } = params;
   return (
     <main className="flex flex-col  h-svh ">
       <HeaderGeneral isBgNone={true} />
-      <video
+      {/* <video
         autoPlay
         loop
         muted
@@ -16,7 +17,8 @@ export default function Home({ params }: any) {
         <source src="/hero.mov" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
-      <HomePage locale={locale} />
+      <HomePage locale={locale} /> */}
+      <Button>Test</Button>
     </main>
   );
 }
