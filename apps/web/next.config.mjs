e74e351cd176b4165454cb125b18f778680a4655
ikbal arslan <ikbalarslandev@gmail.com>
2024-10-14 18:31:49 +0300

    fix prisma bug

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,6 +1,7 @@
 import createNextIntlPlugin from "next-intl/plugin";
-import withPrisma from "@hamampass/db/prisma";
+import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
 
+// Create the withNextIntl plugin
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
@@ -16,6 +17,16 @@ const nextConfig = {
       "utfs.io",
     ],
   },
+  webpack: (config, { isServer }) => {
+    // Add the PrismaPlugin for server-side builds
+    if (isServer) {
+      config.plugins = [...(config.plugins || []), new PrismaPlugin()];
+    }
+
+    // If there are any custom webpack modifications, return the updated config
+    return config;
+  },
 };
 
-export default withNextIntl(withPrisma(nextConfig));
+// Apply withNextIntl and export the final config
+export default withNextIntl(nextConfig);
