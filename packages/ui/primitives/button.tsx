55fc6651ef454030f08f684fd6fd08d74cdb957c
ikbal arslan <ikbalarslandev@gmail.com>
2024-10-15 11:58:57 +0300

    fix components

--- packages/ui/primitives/button.tsx
+++ packages/ui/primitives/button.tsx
@@ -10,6 +10,8 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default: "bg-primary text-primary-foreground hover:bg-primary/90",
+        filter: "border-l rounded-none",
+        load: "bg-cyan-500 text-primary-foreground hover:bg-primary/90",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
@@ -19,7 +21,9 @@ const buttonVariants = cva(
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
         none: "",
-        clear: "",
+        clear: "text-primary underline-offset-4 text-xs absolute top-0 right-2",
+        review:
+          "text-primary underline-offset-4 hover:underline text-cyan-600 ",
       },
       size: {
         default: "h-10 px-4 py-2",
