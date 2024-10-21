6b1111ba39fe115f454c58bfccdc53f0ee847658
ikbal arslan <ikbalarslandev@gmail.com>
2024-10-21 18:13:22 +0300

    refactor(prisma): migrate types for admin side

--- apps/admin/components/admin/index.tsx
+++ apps/admin/components/admin/index.tsx
@@ -3,7 +3,7 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
-import { TProduct } from "@/types";
+import { TProduct } from "@hamampass/db/types";
 import { Button } from "@/components/ui/button";
 import {
   Form,
